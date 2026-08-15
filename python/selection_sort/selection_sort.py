a1e1b3fb6d9593828315f4d3f0ceda9fb9944282
rensantana <santana.renan@outlook.com>
2026-08-15 20:10:16 -0300

    Use English names in selection sort

---
+++ python/selection_sort/selection_sort.py
@@ -0,0 +1,18 @@
+def find_smallest_index(items):
+    smallest = items[0]
+    smallest_index = 0
+    for index in range(1, len(items)):
+        if items[index] < smallest:
+            smallest = items[index]
+            smallest_index = index
+    return smallest_index
+
+def selection_sort(items):
+    sorted_items = []
+    for _ in range(len(items)):
+        smallest_index = find_smallest_index(items)
+        sorted_items.append(items.pop(smallest_index))
+    return sorted_items
+
+
+print(selection_sort([8, 9, 7, 5, 6, 5, 2, 3, 4, 5, 3, 9, 2, 4, 6, 5, 3, 2, 4, 0, 8]))
